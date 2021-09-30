e7c957362535c58178a195a54f9324c6b93bccce
Connor Wilson <cnwilson29@gmail.com>
2021-09-30 16:14:50 -0400

    arrow

--- site/src/Widget.js
+++ site/src/Widget.js
@@ -1,15 +1,22 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 import './Widget.css';
 
 function Widget(props) {
+  const [open, setOpen] = useState(false)
+
   return (
     <div class='py-2'>
         <div class="card shadow-lg pointer" data-target="#collapseExample" style={{
             border: 'none',
             borderRadius: '10px',
             padding: 0
-        }} onClick={props.onClick}>
+        }} onClick={() =>{
+            setOpen(!open);
+            if ('onClick' in props) {
+                props.onClick()
+            }
+        }}>
             <div class="card-body" style={{textAlign: 'left'}} data-toggle="collapse" data-target={"#" + props.title}>
                 <div class='row'>
                     <div class='col-8'>
@@ -17,7 +24,11 @@ function Widget(props) {
                         <p class='widget-subtitle'>{props.subtitle}</p>
                     </div>
                     <div class='col'>
-                        <h1 class='trailing'><img class='trailing-image' src={props.trailing}></img></h1>
+                        <h1 class='trailing'><img class='trailing-image' src={props.trailing} style={{
+                            transitionDuration: '0.3s',
+                            transitionProperty: 'transform',
+                            transform: (open && props.children != undefined) ? 'rotate(90deg)' : ''
+                        }}></img></h1>
                     </div>
                     </div>
                 </div>
